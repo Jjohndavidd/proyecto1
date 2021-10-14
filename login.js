f6c42cc92210335538b53d95d8542fedea75cba6
Jjohndavidd <89837482+Jjohndavidd@users.noreply.github.com>
2021-10-14 09:56:40 +0100

    se realizan algunas modificaciones a los estilos de la página del login y se realiza una propuesta de funciones a espera para validar en el codefunction login(){

--- login.js
+++ login.js
@@ -1,4 +1,7 @@
 let registros = [];
+let rescaptcha = 100;
+let valor = document.getElementById('captcha').value;
+
 function agregarRegistro(){
     let registro = document.getElementsByTagName("input");
         
@@ -11,7 +14,38 @@ function agregarRegistro(){
     }
     registros.push(datosUsuario); 
     console.log(registros);
-}   
+} 
+function login(){
+    const correo = document.getElementById("correo").value; 
+    const contrasena = document.getElementById("contrasena").value;
+    let email;
+    let pass;
+    if(email= registros.find(e => e.correo == correo)){
+
+        if(pass = registros.find(e => e.contrasena == contrasena)){
+          
+            if(validarCAPTCHA(valor)){
+
+            
+            }         
+        } 
+
+    }
+    
+} 
+
+
+
+function validarCAPTCHA(valor){
+    valor = document.getElementById('captcha').value;
+    valor=Number(valor);
+    if(valor===rescaptcha){
+      return true;
+    } 
+    return false;
+  }
 
 module.exports.registros=registros;
 module.exports.agregarRegistro=agregarRegistro;
+module.exports.validarCAPTCHA=validarCAPTCHA;
+module.exports.login=login
